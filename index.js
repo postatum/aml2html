c228ce941c196e0b985b03ae1613b63356c529fd
Artem Kostiuk <postatum@gmail.com>
2019-04-05 12:45:07 +0300

    Init code

---
+++ index.js
@@ -0,0 +1,14 @@
+var amf = require("amf-client-js")
+
+
+return amf.AMF.init()
+  .then(() => {
+    return new amf.Aml10Parser()
+      .parseFileAsync("file://vocabularies/document.yaml")
+  })
+  .then((model) => {
+    return amf.AMF.amfGraphGenerator().generateString(model)
+  })
+  .then((generated) => {
+    console.log(JSON.stringify(JSON.parse(generated), null, 2))
+  })
