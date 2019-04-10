682ffd5536bf336a3d7127b580ba3de5f7080ea5
Artem Kostiuk <postatum@gmail.com>
2019-04-10 07:17:39 +0300

    Rework vocabularies meta info collection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const amf = require('amf-client-js')
 const ldquery = require('ld-query')
 
+const CTX = {
+  amldoc: 'http://a.ml/vocabularies/document#',
+  meta: 'http://a.ml/vocabularies/meta#',
+  owl: 'http://www.w3.org/2002/07/owl#',
+  rdf: 'http://www.w3.org/2000/01/rdf-schema#',
+  schema: 'http://schema.org/'
+}
+
 /* Converts AML Vocabulary to resolved JSON-LD AMF Graph. */
 async function getJsonLdGraph (fpathArg) {
   const model = await new amf.Aml10Parser().parseFileAsync(`file://${fpathArg}`)
@@ -11,23 +19,43 @@ async function getJsonLdGraph (fpathArg) {
   return JSON.parse(graphStrResolved)
 }
 
+/*
+  Outputs array of Vocabularies data which looks like:
+    [
+      ...
+      { id: 'file://test_data/vocabularies/data_model.yaml',
+        base: 'http://a.ml/vocabularies/data#',
+        usage: 'Vocabulary ...',
+        classes: [ 'http://a.ml/vocabularies/data#Node', ... ] }
+      ...
+    ]
+
+  Works on JSON instead of using querying API because the latter picks
+  first object in the order of definition instead of picking root
+  elements first.
+*/
 function collectVocabulariesData (doc) {
-  // TODO
   const vocabularies = doc.queryAll('amldoc:references')
   vocabularies.push(doc)
   return vocabularies.map((voc) => {
+    let vocJson = voc.json()
     return {
-      id: voc.query('@id'),
-      base: voc.query('meta:base @value'),
-      usage: voc.query('amldoc:usage @value'),
-      classes: voc.queryAll('amldoc:declares[@type=owl:Class] @id')
+      id: vocJson['@id'],
+      base: vocJson[`${CTX.meta}base`][0]['@value'],
+      usage: vocJson[`${CTX.amldoc}usage`][0]['@value'],
+      classes: vocJson[`${CTX.amldoc}declares`]
+        .map((decl) => {
+          if (decl['@type'].indexOf(`${CTX.owl}Class`) > -1) {
+            return decl['@id']
+          }
+        })
+        .filter((id) => { return id !== undefined })
     }
   })
 }
 
 /*
   Outputs array of classTerms data which looks like:
-
     [
       ...
       { id: 'http://a.ml/vocabularies/document#DomainElement',
@@ -35,7 +63,7 @@ function collectVocabulariesData (doc) {
         description: 'asd',
         properties:
          [ { name: 'extends',
-             description: 'Target base unit being extended by this extension model',
+             description: 'Target asdasd',
              range: 'http://www.w3.org/2001/XMLSchema#anyUri',
              parent: null } ],
         parents: [] },
@@ -61,7 +89,6 @@ function collectClassesData (doc) {
 
 /*
   Outputs map of propertyTerms ids to its data which looks like:
-
     {
        ...
       'http://a.ml/vocabularies/document#displayName': {
@@ -78,15 +105,11 @@ function collectPropertiesData (doc) {
   const propertyTerms = doc.queryAll(
     'amldoc:declares[@type=owl:DatatypeProperty]')
   propertyTerms.forEach((term) => {
-    propsMap[term.query('@id')] = term
-  })
-  Object.keys(propsMap).forEach((id) => {
-    let prop = propsMap[id]
-    propsMap[id] = {
-      name: prop.query('meta:displayName @value'),
-      description: prop.query('schema:description @value'),
-      range: prop.query('rdf:range @id'),
-      parent: prop.query('rdf:subPropertyOf @id')
+    propsMap[term.query('@id')] = {
+      name: term.query('meta:displayName @value'),
+      description: term.query('schema:description @value'),
+      range: term.query('rdf:range @id'),
+      parent: term.query('rdf:subPropertyOf @id')
     }
   })
   return propsMap
@@ -101,15 +124,10 @@ async function main () {
   const graph = await getJsonLdGraph(process.argv[2])
 
   // Core context used in all the vocabularies
-  const doc = ldquery(graph, {
-    amldoc: 'http://a.ml/vocabularies/document#',
-    meta: 'http://a.ml/vocabularies/meta#',
-    owl: 'http://www.w3.org/2002/07/owl#',
-    rdf: 'http://www.w3.org/2000/01/rdf-schema#',
-    schema: 'http://schema.org/'
-  })
+  let doc = ldquery(graph, CTX)
+  // Pick root vocabulary itself as a single doc
+  doc = doc.query('[@type=meta:Vocabulary]')
   const vocsData = collectVocabulariesData(doc)
-  console.log(vocsData)
   const classesData = collectClassesData(doc)
 }
 
