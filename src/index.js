479c75399e47d50a941046dd6c3579e655f79032
Artem Kostiuk <postatum@gmail.com>
2019-04-24 08:58:15 +0300

    Implement scalar props collection

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const CTX = {
   meta: 'http://a.ml/vocabularies/meta#',
   owl: 'http://www.w3.org/2002/07/owl#',
   rdf: 'http://www.w3.org/2000/01/rdf-schema#',
-  schema: 'http://schema.org/'
+  schema: 'http://schema.org/',
+  shacl: 'http://www.w3.org/ns/shacl#'
 }
 
 // function collectVocabulariesData (doc) {
@@ -123,8 +124,8 @@ async function main () {
   fs.emptyDirSync(outDir)
 
   // Collects dialects data into an array
-  const dialectsPaths = Array.isArray(args._) ? args._ : [args._]
-  const dialectsData = dialectsPaths.map((dpth) => {
+  const dialectsPaths = Array.isArray(argv._) ? argv._ : [argv._]
+  const dialectsData = dialectsPaths.map(async (dpth) => {
     let graph = await utils.getJsonLdGraph(dpth)
     let doc = ldquery(graph, CTX).query('[@type=meta:Dialect]')
     console.log(`Collecting dialect data: ${dpth}`)
@@ -157,6 +158,49 @@ async function main () {
   utils.copyCss(outDir)
 }
 
+/*
+# dialectsData
+  [
+    # dialectData
+    {
+      name: 'WebAPI',
+      htmlName: 'webapi.html',
+      id: 'file://test_data/dialects/canonical_webapi.yaml',
+      nodeMappings: [
+
+        # nodeData
+        {
+          name: 'Request',
+          htmlName: 'node_request.html',
+          id: 'http://a.ml/vocabularies/http#Request',
+          description: 'Request information for an operation',
+          scalarProperties: [
+            {
+              name: 'description',
+              id: 'http://schema.org/description',
+              range: 'string',
+              mandatory: false,
+              otherProperty: 'value',
+              ...
+            }
+          ],
+          linkProperties: [
+            {
+              name: 'parameter',
+              id: 'http://a.ml/vocabularies/http#parameter',
+              range: 'Parameter',
+              mandatory: true,
+              otherProperty: 'value',
+              ...
+            }
+          ]
+        },
+        ...
+      ]
+    },
+    ...
+  ]
+*/
 function collectDialectData (doc) {
   const dialectData = {
     name: doc.query('schema:name @value'),
@@ -169,6 +213,87 @@ function collectDialectData (doc) {
 }
 
 function collectNodesData (doc) {
+  const nodes = doc.queryAll('shacl:Shape').map((node) => {
+    // name, id
+    let nodeData = {
+      name: node.query('schema:name @value'),
+      id: node.query('@id')
+    }
+    // description
+    let targetClassId = node.query('shacl:targetClass @id')
+    let targetClass = doc.query(`[@id=${targetClassId}]`)
+    nodeData.description = targetClass.query('schema:description @value')
+    // htmlName
+    const slug = nodeData.name.split(' ').join('').toLowerCase()
+    nodeData.htmlName = `node_${slug}.html`
+    nodeData.scalarProperties = collectScalarPropsData(doc, node)
+    nodeData.linkProperties = collectLinkScalarPropsData(doc, node)
+    return nodeData
+  })
+  return nodes
+}
+
+/*
+Scalar properties DO contain 'shacl:datatype' node
+
+  scalarProperties: [
+    {
+      name: 'description',
+      id: 'http://schema.org/description',
+      range: 'string',
+      constraints: [{name: 'pattern', value: 'asdasd'}, ...]
+    }
+  ]
+*/
+function collectScalarPropsData (doc, node) {
+  const propsNodes = node.query('shacl:property').filter((prop) => {
+    return !!prop.query('shacl:datatype')
+  })
+  const props = propsNodes.map((prop) => {
+    let propData = {
+      name: prop.query('schema:name @value'),
+      id: prop.query('shacl:path @id'),
+      range: parseHashValue(prop.query('shacl:datatype @id')),
+      constraints: [
+        {name: 'mandatory', value: prop.query('shacl:minCount @value') > 0},
+        {name: 'pattern', value: prop.query('shacl:pattern @value')},
+        {name: 'minimum', value: prop.query('shacl:minInclusive @value')},
+        {name: 'maximum', value: prop.query('shacl:maxInclusive @value')},
+        {name: 'enum', value: prop.query('shacl:in @value')},
+        {name: 'allowMultiple', value: prop.query('meta:allowMultiple @value')},
+        {name: 'sorted', value: prop.query('meta:sorted @value')},
+        {name: 'mapKey', value: prop.query('meta:mapProperty @id')},
+        {name: 'typeDiscriminator',
+         value: prop.query('meta:typeDiscriminatorMap @value')
+                    .split(',').join('\n')},
+        {name: 'typeDiscriminatorName',
+         value: prop.query('meta:typeDiscriminatorName @value')}
+      ]
+    }
+    propData.constraints = propData.constraints.filter((con) => {
+      return !!con.value
+    })
+    return propData
+  })
+  return props
+}
+
+/*
+Link properties DON'T contain 'shacl:datatype' node
+
+  linkProperties: [
+    {
+      name: 'parameter',
+      id: 'http://a.ml/vocabularies/http#parameter',
+      range: 'Parameter',
+      constraints: [{name: 'pattern', value: 'asdasd'}, ...]
+    }
+  ]
+*/
+function collectLinkPropsData (doc, node) {
+  const propsNodes = node.query('shacl:property').filter((prop) => {
+    return !prop.query('shacl:datatype')
+  })
 
 }
 
@@ -215,47 +340,3 @@ function collectNavData (dialectData, commonNavData) {
 }
 
 main()
-
-/*
-# dialectsData
-[
-  # dialectData
-  {
-    name: 'WebAPI',
-    htmlName: 'webapi.html',
-    id: 'file://test_data/dialects/canonical_webapi.yaml',
-    nodeMappings: [
-
-      # nodeData
-      {
-        name: 'Request',
-        htmlName: 'node_request.html',
-        id: 'http://a.ml/vocabularies/http#Request',
-        description: 'Request information for an operation',
-        scalarProperties: [
-          {
-            name: 'description',
-            id: 'http://schema.org/description',
-            range: 'string',
-            mandatory: false,
-            otherProperty: 'value',
-            ...
-          }
-        ],
-        linkProperties: [
-          {
-            name: 'parameter',
-            id: 'http://a.ml/vocabularies/http#parameter',
-            range: 'Parameter',
-            mandatory: true,
-            otherProperty: 'value',
-            ...
-          }
-        ]
-      },
-      ...
-    ]
-  },
-  ...
-]
-*/
