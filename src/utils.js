7dc0ea7a07d776636c47a4320e81696822c46fbc
Artem Kostiuk <postatum@gmail.com>
2019-10-02 10:45:17 +0300

    Remove turndown

--- src/utils.js
+++ src/utils.js
@@ -71,14 +71,6 @@ function renderTemplate (data, tmplPath, outPath) {
   fs.writeFileSync(outPath, renderedStr)
 }
 
-function convertToMarkdown (htmlStr) {
-  const TurndownService = require('turndown')
-  const turndownPluginGfm = require('turndown-plugin-gfm')
-  const turndownService = new TurndownService()
-  turndownService.use(turndownPluginGfm.gfm)
-  return turndownService.turndown(htmlStr)
-}
-
 function nameSorter (a, b) {
   if (a.name > b.name) {
     return 1
